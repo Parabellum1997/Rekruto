59675b0645a937d17d46ae7a42b6d16e80718b3a
Parabellum1997 <belihshura@yandex.ru>
2022-03-10 10:56:28 +0300

    Второй коммит

--- Site/TZ/urls.py
+++ Site/TZ/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('next',views.next)
+    path('',views.index, name='home'),
+    path('name=Rekruto&message=Давайдружить!',views.next, name='Rekruto&message=Давайдружить!')
 ]
